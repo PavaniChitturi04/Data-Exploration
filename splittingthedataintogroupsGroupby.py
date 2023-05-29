b1eca88d443d13f3dd6ca2762337b722a359edb6
PavaniChitturi04 <130127616+PavaniChitturi04@users.noreply.github.com>
2023-05-29 22:07:45 +0530

    Update splittingthedataintogroupsGroupby.py

--- splittingthedataintogroupsGroupby.py
+++ splittingthedataintogroupsGroupby.py
@@ -1,15 +1,37 @@
 import pandas as pd
 
-# Create a DataFrame
+# Create a sample DataFrame
 df = pd.DataFrame({
-    'Category': ['A', 'B', 'A', 'B', 'A', 'B'],
-    'Value': [1, 2, 3, 4, 5, 6]
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [25, 32, 28, 31, 24],
+    'City': ['New York', 'Los Angeles', 'New York', 'Chicago', 'Los Angeles'],
+    'Salary': [5000, 6000, 5500, 7000, 4500]
 })
 
-# Group the DataFrame by 'Category'
-grouped = df.groupby('Category')
+#print("Grouping data by 'City' column")
+#grouped = df.groupby('City')
 
-# Calculate the sum of each group
-sums = grouped.sum()
+# Accessing groups
+#for city, group in grouped:
+ #   print(f'City: {city}')
+ #   print(group)
+  #  print('---')
 
-print(sums)
+print("\n","Grouping by multiple columns")
+grouped = df.groupby(['City', 'Age'])
+
+# Accessing groups with multiple keys
+for (city, age), group in grouped:
+    print(f'City: {city}, Age: {age}')
+    print(group)
+    print('---')
+
+# Applying aggregation functions on grouped data
+grouped = df.groupby('City')
+mean_salary = grouped['Salary'].mean()
+median_age = grouped['Age'].median()
+
+print("\n",'Mean Salary by City:')
+print(mean_salary)
+print("\n",'Median Age by City:')
+print(median_age)
